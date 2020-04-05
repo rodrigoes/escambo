754e582255e7cc2297604e32691b64d47428037e
Rafael Santana <rafaellauton@outlook.com>
2020-04-05 15:39:50 -0300

    testes para busca de item

--- src/main/java/facens/engsoft/escambo/services/ItemService.java
+++ src/main/java/facens/engsoft/escambo/services/ItemService.java
@@ -4,10 +4,23 @@ import facens.engsoft.escambo.models.Item;
 import facens.engsoft.escambo.models.Usuario;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class ItemService {
 
     public boolean addItemPara(Usuario usuario, Item item) {
         return false;
+    }
+
+    public List<Item> buscarItensPorNome(String name) {
+        Item itemA = new Item();
+        itemA.setNome("Vaso de Petunias");
+
+        Item itemB = new Item();
+        itemB.setNome("Toalha");
+
+        return Arrays.asList(itemA, itemB);
     }
 }
--- src/test/java/facens/engsoft/escambo/services/ItemServiceTest.java
+++ src/test/java/facens/engsoft/escambo/services/ItemServiceTest.java
@@ -4,8 +4,10 @@ import facens.engsoft.escambo.models.Item;
 import facens.engsoft.escambo.models.Usuario;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.any;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class ItemServiceTest {
 
@@ -20,7 +22,32 @@ public class ItemServiceTest {
         item.setNome("Coração de Ouro");
         item.setDescricao(any());
 
-        boolean actual = !itemService.addItemPara(usuario, item);
+        boolean actual = itemService.addItemPara(usuario, item);
         assertTrue(actual, "Testa que cadastro de item foi concretizado para usuário");
+    }
+
+    @Test
+    public void verificaBuscaDeItensPorNome_quando_EstiverCadastrado() {
+        Item expectedItem = new Item();
+        expectedItem.setNome("Toalha");
+
+        List<Item> actualItems = itemService.buscarItensPorNome(expectedItem.getNome());
+
+        boolean itensTemNomeEsperado = actualItems
+                .stream()
+                .allMatch(item -> item.getNome().toLowerCase().contains(expectedItem.getNome().toLowerCase()));
+
+        assertTrue(!actualItems.isEmpty() && itensTemNomeEsperado,
+                "Lista retornada deve conter elementos compatíveis");
+    }
+
+    @Test
+    public void verificaBuscaDeItensPorNome_quando_NaoEstiverCadastrado() {
+        Item expectedItem = new Item();
+        expectedItem.setNome("Nave Vógon");
+
+        List<Item> actualItems = itemService.buscarItensPorNome(expectedItem.getNome());
+
+        assertTrue(actualItems.isEmpty(), "A lista retornada deve ser vazia");
     }
 }
