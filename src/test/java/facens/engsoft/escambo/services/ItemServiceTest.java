3d851b3880955a1dd4763ea4883f4e5bc45fc4a1
Rafael Santana <rafaellauton@outlook.com>
2020-04-27 22:40:15 -0300

    print de exemplo de stub em testes adicionado em doc

--- src/test/java/facens/engsoft/escambo/services/ItemServiceTest.java
+++ src/test/java/facens/engsoft/escambo/services/ItemServiceTest.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
 
 @SpringBootTest
 public class ItemServiceTest {
